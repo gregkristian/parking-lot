2dabecdbd86f046348fdaadb48cc6549c5e221e5
gregkristian <gkristianp@gmail.com>
2020-01-19 09:23:05 +0000

    Initial commit

---
+++ src/parkinglotgojek/ParkingApp.java
@@ -0,0 +1,19 @@
+package parkinglotgojek;
+
+
+import java.util.Scanner;
+
+/**
+ * Application to manage parking lot
+ *
+ * @author Gregorius Purwidi
+ */
+public class ParkingApp {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
