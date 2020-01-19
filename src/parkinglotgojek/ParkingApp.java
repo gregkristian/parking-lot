cd952dccececc9fbd552fcfe0d98fe7991bcd14f
gregkristian <gkristianp@gmail.com>
2020-01-19 10:20:05 +0000

    Add stdin. Add method to create lot and print status

--- src/parkinglotgojek/ParkingApp.java
+++ src/parkinglotgojek/ParkingApp.java
@@ -14,6 +14,29 @@ public class ParkingApp {
      * @param args
      */
     public static void main(String[] args) {
+        //Read stdin until exit
+        //TODO check support in command line
+        ParkingLot parkingLot = null;
+        boolean keepRun = true;
 
+        try (Scanner in = new Scanner(System.in)) {
+            while (keepRun) {
+                String[] values = in.nextLine().split("\\s+");;
+
+                if (values[0].equals("create_parking_lot")) {
+                    parkingLot = new ParkingLot(Integer.parseInt(values[1])); //TODO handle invalid args
+                }
+                else if (values[0].equals("status")) {
+                    parkingLot.printStatus();
+                }
+                else if (values[0].equals("exit")) {
+                    keepRun = false;
+                }
+                else {
+                    System.out.printf("Unknown command: %s \n", values[0]);
+                }
+            }
+        } // TODO add catch?
+        System.out.println("Exiting..."); //TODO rm this
     }
 }
--- src/parkinglotgojek/ParkingLot.java
+++ src/parkinglotgojek/ParkingLot.java
@@ -3,6 +3,11 @@ package parkinglotgojek;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Parking lot object
+ *
+ * @author Gregorius Purwidi
+ */
 public class ParkingLot {
 
     private Map<Integer, Car> parkingLot;
@@ -14,5 +19,16 @@ public class ParkingLot {
             parkingLot.put(Integer.valueOf(i), null);
         }
         System.out.printf("Created a parking lot with %d slots\n", parkingLot.size());
+    }
+
+    public void printStatus() {
+        System.out.printf("Slot No. Registration No Colour \n");
+        for (Map.Entry<Integer, Car> entry : parkingLot.entrySet())
+        {
+            Car v = entry.getValue();
+            if (v != null) {
+                System.out.printf("%d %s %s\n", entry.getKey(), v.getRegPlate(), v.getColor());
+            }
+        }
     }
 }
