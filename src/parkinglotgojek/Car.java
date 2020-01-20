ddd6093cab955b2e35f98f8bba19871cfd76a9b5
gregkristian <gkristianp@gmail.com>
2020-01-20 00:40:58 +0000

    Refactor tabs

--- src/parkinglotgojek/Car.java
+++ src/parkinglotgojek/Car.java
@@ -6,20 +6,20 @@ package parkinglotgojek;
  * @author Gregorius Kristian Purwidi
  */
 public class Car {
-	private String regPlate; // Registration plate
-	private String colour;
+    private String regPlate; // Registration plate
+    private String colour;
 
-	public Car(String regPlate, String colour) {
-	    //TODO handle invalid args?
-		this.regPlate = regPlate;
-		this.colour = colour;
-	}
+    public Car(String regPlate, String colour) {
+        //TODO handle invalid args?
+        this.regPlate = regPlate;
+        this.colour = colour;
+    }
 
-	public String getRegPlate() {
-		return regPlate;
-	}
+    public String getRegPlate() {
+        return regPlate;
+    }
 
-	public String getColour() {
-		return colour;
-	}
+    public String getColour() {
+        return colour;
+    }
 }
